6d214a0e7e94025b3bc8f41bd02d26ab0a6509ac
tofu345 <shammahayotunde1@gmail.com>
2025-09-18 15:31:23 +0100

    changed gaps, selected border colour

--- config.h
+++ config.h
@@ -5,10 +5,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 3;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 3;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 3;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 3;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 2;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 2;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 2;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 2;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -32,11 +32,11 @@ static const char col_brblk[]  = "#444b6a";  // bright black
 static const char *colors[][3] = {
     /*                      fg          bg          border */
     [SchemeNorm]        = { col_fg,     col_bg,     col_blk },
-    [SchemeSel]         = { col_wht,    col_bg,     col_mag },
+    [SchemeSel]         = { col_wht,    col_bg,     col_brblk },
     [SchemeStatus]      = { col_wht,    col_bg,     "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
     [SchemeTagsSel]     = { col_wht,    col_bg,     "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
     [SchemeTagsNorm]    = { col_fg,     col_bg,     "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
-    [SchemeInfoSel]     = { col_fg,     col_bg,     "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+    [SchemeInfoSel]     = { col_wht,    col_bg,     "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
     [SchemeInfoNorm]    = { col_fg,     col_bg,     "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
@@ -121,8 +121,8 @@ static const Key keys[] = {
     { MODKEY,                       XK_b,                       togglebar,      {0} },
     { MODKEY,                       XK_j,                       focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,                       focusstack,     {.i = -1 } },
-    { MODKEY,                       XK_i,                       incnmaster,     {.i = +1 } },
-    { MODKEY,                       XK_p,                       incnmaster,     {.i = -1 } },
+    { MODKEY,                       XK_p,                       incnmaster,     {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_p,                       incnmaster,     {.i = -1 } },
     { MODKEY,                       XK_h,                       setmfact,       {.f = -0.05 } },
     { MODKEY,                       XK_l,                       setmfact,       {.f = +0.05 } },
 
@@ -144,7 +144,6 @@ static const Key keys[] = {
     { MODKEY|Mod1Mask,              XK_9,                       incrovgaps,     {.i = +1 } },
     { MODKEY|Mod1Mask|ShiftMask,    XK_9,                       incrovgaps,     {.i = -1 } },
 
-
     // { MODKEY,                       XK_z,                       zoom,           {0} },
     { MODKEY,                       XK_Tab,                     view,           {0} },
     { MODKEY|ShiftMask,             XK_q,                       killclient,     {0} },
@@ -156,7 +155,7 @@ static const Key keys[] = {
     { 0,			    XF86XK_MonBrightnessUp,	spawn,	        {.v = light_up} },
     { 0,		    	    XF86XK_MonBrightnessDown,	spawn,	        {.v = light_down} },
 
-    { MODKEY,                       XK_t,                       setlayout,      {.v = &layouts[0]} },
+    { MODKEY,                       XK_t,                       setlayout,      {.v = &layouts[0]} }, // tiled
     { MODKEY,                       XK_f,                       setlayout,      {.v = &layouts[1]} }, // monocle
     { MODKEY,                       XK_space,                   setlayout,      {0} },
     { MODKEY|ShiftMask,             XK_space,                   togglefloating, {0} },
@@ -168,7 +167,7 @@ static const Key keys[] = {
     { MODKEY|ShiftMask,             XK_comma,                   tagmon,         {.i = -1 } },
     { MODKEY|ShiftMask,             XK_period,                  tagmon,         {.i = +1 } },
 
-    // real prog dvorak L
+    // real prog dvorak L https://github.com/ThePrimeagen/keyboards
     TAGKEYS(XK_plus,         0),
     TAGKEYS(XK_bracketleft,  1),
     TAGKEYS(XK_braceleft,    2),
