c677d9fd3fe1e0a29bafe8770c1856533fccd813
tofu345 <shammahayotunde1@gmail.com>
2026-05-13 19:46:52 +0100

    patch:unicode_ellipsis-20222909-d3f93c7

--- drw.c
+++ drw.c
@@ -261,7 +261,7 @@ drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned int lp
 
 	usedfont = drw->fonts;
 	if (!ellipsis_width && render)
-		ellipsis_width = drw_fontset_getwidth(drw, "...");
+		ellipsis_width = drw_fontset_getwidth(drw, "…");
 	if (!invalid_width && render)
 		invalid_width = drw_fontset_getwidth(drw, invalid);
 	while (1) {
@@ -323,7 +323,7 @@ drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned int lp
 			w -= invalid_width;
 		}
 		if (render && overflow)
-			drw_text(drw, ellipsis_x, y, ellipsis_w, h, 0, "...", invert);
+			drw_text(drw, ellipsis_x, y, ellipsis_w, h, 0, "…", invert);
 
 		if (!*text || overflow) {
 			break;
