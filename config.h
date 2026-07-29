2a7012dca09cc5e5e1df410969ab4a5e6fde7b1b
tofu345 <shammahayotunde1@gmail.com>
2026-07-29 12:02:16 +0100

    patch:dwm-swallow

--- config.h
+++ config.h
@@ -7,6 +7,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing = 3;   /* systray spacing */
@@ -47,10 +48,10 @@ static const Rule rules[] = {
      *  WM_CLASS(STRING) = instance, class
      *  WM_NAME(STRING) = title
      */
-    /* class            instance    title       tags mask     iscentered    isfloating   monitor */
-    { "Nemo",           NULL,       NULL,       0,            1,            1,           -1 },
-    { "Gcr-prompter",   NULL,       NULL,       0,            1,            1,           -1 },
-    { "soteria",        NULL,       NULL,       0,            1,            1,           -1 },
+    /* class            instance    title       tags mask     iscentered    isfloating  isterminal   noswallow  monitor */
+    { "Nemo",           NULL,       NULL,       0,            1,            1,          0,           1,         -1 },
+    { "Gcr-prompter",   NULL,       NULL,       0,            1,            1,          0,           1,         -1 },
+    { "Alacritty",      NULL,       NULL,       0,            0,            0,          1,           0,         -1 },
 };
 
 /* layout(s) */
